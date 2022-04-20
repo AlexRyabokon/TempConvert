f8b09a26cdd459f488d522939648946a6fee4109
Alexander Ryabokon <leonid555100@gmail.com>
2022-04-20 14:00:46 +0300

    four commit

--- src/main/java/org/temperature/TextArrs.java
+++ src/main/java/org/temperature/TextArrs.java
@@ -2,7 +2,7 @@ package org.temperature;
 
 public class TextArrs {
 
-    String[] arrayStrings = {"Отец мой Андрей Петрович Гринев",                                                                      //0
+    public String[] arrayStrings = {"Отец мой Андрей Петрович Гринев",                                                                      //0
             "паче всякого чаяния матушка родила дочь, то батюшка объявил бы куда следовало о смерти",                                //1
             "С пятилетнего возраста отдан я был на руки стремянному Савельичу, за трезвое поведение",                                //2
             "Бопре, принялся за работу",                                                                                             //3
@@ -13,18 +13,17 @@ public class TextArrs {
             "На другой день поутру подвезена была к крыльцу",                                                                        //8
             "Ты едешь в Оренбург служить под его начальством",                                                                       //9
             "На другой день поутру подвезена была к крыльцу дорожная кибитка"                                                        //10
-                            };
+    };
 
-
+    public double av = averageStrLength();
 
     //4. Найти самую короткую и самую длинную строки. Вывести найденные строки и их длину.
-    public void sravn(){
+    public void sravn() {
 
-       String stringMax = arrayStrings[0];
+        String stringMax = arrayStrings[0];
         String stringMin = arrayStrings[0];
 
-        for (int i = 0; i < arrayStrings.length; i++)
-        {
+        for (int i = 0; i < arrayStrings.length; i++) {
             if (arrayStrings[i].length() > stringMax.length()) {
                 stringMax = arrayStrings[i];
             }
@@ -58,9 +57,60 @@ public class TextArrs {
 
     }
 
+    // 6. Вывести на консоль те строки, длина которых меньше (больше) средней арифметической всех длин строк в массиве, а также длину.
+    public double averageStrLength() {
+        double average = (arrayStrings[0].length() + arrayStrings[1].length() + arrayStrings[2].length() +
+                arrayStrings[3].length() + arrayStrings[4].length() +
+                arrayStrings[5].length() +
+                arrayStrings[6].length() + arrayStrings[7].length() +
+                arrayStrings[8].length() + arrayStrings[9].length() +
+                arrayStrings[10].length()) / arrayStrings.length;
+        return average;
+    }
 
+    public void compareLength() {
+        System.out.println(" \n Строки длинной больше ср. ар.:  \n");
+        for (int i = 0; i < arrayStrings.length; i++) {
+            if (arrayStrings[i].length() > av)
+                System.out.println(arrayStrings[i]);
+        }
+        System.out.println("\n  Строки длинной меньше ср. ар.:  \n");
+        for (int j = 0; j < arrayStrings.length; j++) {
+            if (arrayStrings[j].length() < av)
+                System.out.println(arrayStrings[j]);
+        }
 
+    }
+
+    //7. Найти слово, символы в котором идут в строгом порядке возрастания их кодов.Если таких слов несколько, найти первое из них.
+    public static String[] separateString(String str) {
+        String delimeter = " "; // Разделитель
+        String[] subStr = str.split(delimeter);
+        return subStr;
+
+    }
+
+    public static char[] toSymbolsWord(String str){
+        char[] str1 = str.toCharArray();
+        return str1;
+
+    }
+
+public void someMeth() {
+    for (int i = 0; i < arrayStrings.length; i++) {
+        String[] arrayOfchars = TextArrs.separateString(arrayStrings[i]); // слова в String
+
+        for (int j = 0; j < arrayOfchars.length; j++) {
+            char[] c = toSymbolsWord(arrayOfchars[j]); // слова в массиве char
+            int charCode = (int) c[j];
+            System.out.println(charCode); //zatup
+
+             }
+        }
+    }
 }
+
+
 
 
 
@@ -68,9 +118,9 @@ public class TextArrs {
 class Realization1{
 
     public static void main(String[] args) {
-        TextArrs t = new TextArrs();
 
-
+    TextArrs t = new TextArrs();
+    t.someMeth();
 
 
     }
